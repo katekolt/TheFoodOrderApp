35cfb85ee8eb1503a75afea554ac920b579ab967
Kateryna Koltovska <79753036+katekolt@users.noreply.github.com>
2022-03-22 15:49:30 +0200

    Add new components and logic

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,21 @@
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/img.jpg'
 import classes from './Header.module.css';
-import {Fragment} from "react";
-import mealsImage from '../../assets/img.jpg';
-import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
-    return (<Fragment>
-        <header className={classes.header}>
-            <h1>ReactMeals</h1>
-            <HeaderCartButton />
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImage} alt='A table full a delicious food!'/>
-        </div>
-    </Fragment>)
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton onClick={props.onShowCart} />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt='A table full of delicious food!' />
+            </div>
+        </Fragment>
+    );
 };
+
 export default Header;
